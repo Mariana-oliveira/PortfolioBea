da53c1a72e55cf72439928d0a91334ac37247667
Mariana <mariana_oliveira_9@hotmail.com>
2020-08-11 15:11:01 +0100

    main page desktop version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { IntroComponent } from './main/intro/intro.component';
+import { AreasComponent } from './main/areas/areas.component';
+import { AreaComponent } from './main/areas/area/area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    IntroComponent,
+    AreasComponent,
+    AreaComponent
   ],
   imports: [
     BrowserModule,
